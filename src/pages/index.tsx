0bb4cb9e1cae830c74342bc5e26f42faea515b13
Ross <ross@duris.io>
2022-08-09 21:28:13 -0400

    Updated banner more

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next"
 import Header from '../components/Header'
 import Link from "next/link";
+import Image from "next/image";
 import Head from "next/head" 
 import Footer from "../components/Footer";
 import { ChevronRightIcon, ChevronLeftIcon, DownloadIcon, ChatIcon, CameraIcon } from '@heroicons/react/solid'
@@ -22,18 +23,25 @@ const Home: NextPage = () => {
   return (
   <>
     <Header/> 
-    <main>
-    <div className="sm:border-r sm:border-l -z-30 max-w-7xl relative">
+    <main className="border-l border-r">
+    <div className=" -z-30 max-w-7xl relative">
 
 
 
-      <div className="bg-trasnparent -z-40 w-full h-80 sm:h-96 lg:h-[350px] border-b relative overflow-hidden inline-block"> 
-            <div className="bgElement -z-40 bg-gradient-to-l from-slate-200 to-white w-[900px] h-[900px] rounded-[500px] absolute -bottom-[50px] -right-[300px] hover:-right-[320px] transition-all duration-300"></div>
+      <div className="bg-trasnparent  -z-40 w-full h-80 sm:h-96 lg:h-[350px] border-b relative overflow-hidden inline-block transition-all duration-300"> 
+            <div className="bgElement  -z-40 bg-gradient-to-l from-slate-200 to-white w-[900px] h-[900px] rounded-[500px] absolute -bottom-[50px] -right-[300px] hover:-right-[320px] transition-all duration-300"></div>
+            <div className=" absolute  right-0 -top-14 sm:-top-6 lg:-top-10 opacity-20 sm:opacity-100 transition-all duration-300">
+            <Image 
+              src="https://ik.imagekit.io/zhmk2wx4u/cloudstackbig_TgOutz7x_.png?ik-sdk-version=javascript-1.4.3&updatedAt=1660093604664" 
+              height={500}
+              width={520}
+            />
+            </div>
             {/* <ChevronLeftIcon className="h-16 bottom-32 z-20 text-gray-300 absolute left-1 hover:-left-1 transition-all duration-300 cursor-pointer hover:text-gray-200"/>
             <ChevronRightIcon className="h-16 bottom-32 z-20 text-gray-300 absolute right-1 hover:-right-1 transition-all duration-300 cursor-pointer hover:text-gray-200"/> */}
             
             <div className="sm:ml-10 relative h-72 ml-[26px] -z-0">
-              <p className=" sm:text-4xl font sm:max-w-xl z-10 relative font-extrabold tracking-tight text-gray-900 text-2xl max-w-xs sm:p-10 pt-6 p-10">Great performing websites built on a modern technology stack.<br/>
+              <p className=" sm:text-4xl font sm:max-w-xl z-10 relative sm:bg-white transition-all duration-300 sm:bg-opacity-60 md:bg-transparent font-extrabold tracking-tight text-gray-900 text-2xl max-w-xs sm:p-10 pt-6 p-10">Great performing websites built on a modern technology stack.<br/>
               {/* <Link href="/about"><span className="text-blue-500 hover:text-blue-300 
               cursor-pointer font-extralight underline text-[16px] hover:ml-1 
               transition-all duration-300">Learn More</span></Link> */}
@@ -45,6 +53,8 @@ const Home: NextPage = () => {
             </div>
         </div>      
     </div>
+
+    
       <div className="max-w-7xl mx-auto font-bold rounded relative">
         <button className="bg-blue-400 z-50 hover:bg-blue-300 cursor-pointer text-white absolute left-16 sm:left-20 -top-20 hover:-mt-1 transition-all duration-300 rounded-lg">
               <Link href="/contact">          
@@ -54,7 +64,11 @@ const Home: NextPage = () => {
           </button>
           </div>
 
-      <div className="h-[900px] border-r border-l">
+          <div className="text-gray-700 border-b p-10 font-thin">
+          <p>Modern technology stacks give the best performance possible. </p>
+        </div>
+
+      <div className="h-[900px]">
 
         <div className="grid grid-cols-2 x sm:grid-cols-4 pt-2 mx-4 bg-white text-center welcomeFeature">
         <span><h2> </h2><DeviceMobileIcon className="fIcon"/><div className="feature">Fully responsive desgins that work on all devices.</div></span>
@@ -65,17 +79,13 @@ const Home: NextPage = () => {
           <span><h2></h2><PencilIcon className="fIcon"/><div className="feature">Designs from sratch made to look however you want.</div></span>
           <span><h2></h2><CodeIcon className="fIcon"/><div className="feature">Custom coded with a modern stack using NexJS.</div></span>
           <span><h2> </h2><CogIcon className="fIcon"/><div className="feature">Customized full stack features availabe at request.</div></span>
-          <span><h2> </h2><LockClosedIcon className="fIcon"/><div className="feature">Encrypted by a 256 bit SSL certificate from Vercel.</div></span>
+          <span><h2> </h2><LockClosedIcon className="fIcon"/><div className="feature">Secured with 256 bit SSL encryption from Vercel.</div></span>
         
         </div>
         
         
         
-        <div className="text-white">
-          <ul>
-            <li className="contentFeature">Content 1</li>
-          </ul>
-        </div>
+       
 
         <Footer />
       </div>
