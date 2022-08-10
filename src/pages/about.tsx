e4d80d96ff8548c44e6543ceff62a7ba0c39a4fa
Ross <ross@duris.io>
2022-08-09 20:01:09 -0400

    About page image size

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -9,19 +9,19 @@ export default function About() {
   <>
  <Header/>
  <main>
- <div className="text-4xl text-center text-gray-500 border-b">
+ <div className="text-4xl text-center text-gray-500 border-b mx-auto">
   <h1 className="py-10 pb-4">About</h1>
   <h2 className=" text-lg py-4 pb-10 font-thin">Continuous learning development approach</h2>
   
   </div>
-
-  <div className=" float-left m-10">
+<div className="grid grid-cols-1 sm:grid-cols-2 mx-auto float-none">
+  <div className=" float-right m-10">
   <Image 
       src="https://ik.imagekit.io/zhmk2wx4u/Screen_Shot_2022-08-09_at_4.53.43_PM_WT1r4A7ov.png?ik-sdk-version=javascript-1.4.3&updatedAt=1660078449773" 
-      width={330} height={330} 
+      width={630} height={330} 
       alt="showcase website" 
       objectFit="cover"
-      className="bg-gray-100 rounded-lg shadow-sm" 
+      className="bg-gray-100 rounded-lg shadow-sm float-right inline-block" 
       />      
   </div>
   <div>
@@ -31,6 +31,8 @@ export default function About() {
   <p className="float-left max-w-md p-10 pt-2">
     {secondString}
   </p>
+  </div>
+
   </div>
  </main>
  <Footer />
